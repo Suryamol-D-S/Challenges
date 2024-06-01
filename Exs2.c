857858a21e660788357ddd4535b70544db0637d6
Suryamol <suyamol.ds@mettlenetworks.com>
2024-06-01 16:50:22 +0530

    Challeng1.c file committed

---
+++ Exs2.c
@@ -0,0 +1,10 @@
+//Try to imagine what happens when i has value 0 and is decremented by means of operators
+
+# include <stdio.h>
+int main() {
+    int i=0;
+    printf("Before i increment : %d\n",i);
+    i--;
+    printf("After i increment : %d\n",i);
+    return 0;
+}
