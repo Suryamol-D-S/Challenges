c60ba84e19ad51312ed24273828c00f5412c8381
Suryamol <suyamol.ds@mettlenetworks.com>
2024-06-03 15:42:16 +0530

    docs
    Inserted the detailed description of program with Output for the reference in 01-merge-sort.c

--- challanges/01-merge-sort.c
+++ challanges/01-merge-sort.c
@@ -1,63 +1,95 @@
+/* Merge Sort using recursion
+-------------------------------
+Merge sort is a sorting algorithm that follows the divide-and-conquer approach. 
+It works by recursively dividing the input array into smaller subarrays and sorting 
+those subarrays then merging them back together to obtain the sorted array
+-
+-
+-
+Output :-
+suryanol@Elanor:~/Desktop/Challenges/challanges$ gcc -o 01-merge-sort 01-merge-sort.c 
+suryanol@Elanor:~/Desktop/Challenges/challanges$ ./01-merge-sort 
+Enter the number of elements: 10
+Enter the elements:
+3 5 1 8 9 10 0 3 6 2
+
+Sorted array is 
+0 1 2 3 3 5 6 8 9 10 */
+
 #include <stdio.h>
-int arr[20]; // array to be sorted
-int merge(int arr[],int l,int m,int h);
-int merge_sort(int arr[],int low,int high);
-int main()
-{
-    int n,i;
 
-    printf("Enter the size of array\n"); // input the elements
-    scanf("%d",&n);
-    printf("Enter the elements:");
-    for(i=0; i<n; i++) {
-    scanf("%d",&arr[i]);
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+
+    int L[n1], R[n2];
+
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0; 
+    j = 0; 
+    k = l; 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    merge_sort(arr,0,n-1); // sort the array
 
-    printf("Sorted array:"); // print sorted array
-    for(i=0; i<n; i++) {
-    printf("%d ",arr[i]);
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Merge sort function
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+// Main Code
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("\nSorted array is \n");
+    for(int i=0 ; i<n ; i++) {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
-int merge(int arr[],int l,int m,int h)
-{
-    int arr1[10],arr2[10]; // Two temporary arrays to hold the two arrays to be merged
-
-    int n1,n2,i,j,k;
-    n1=m-l+1;
-    n2=h-m;
-
-    for(i=0; i<n1; i++) {
-    arr1[i]=arr[l+i];
-    }
-    for(j=0; j<n2; j++) {
-    arr2[j]=arr[m+j+1];
-    }
-    //arr1[i]=9999; // To mark the end of each temporary array
-    //arr2[j]=9999;
-
-    i=0;
-    j=0;
-    for(k=l; k<=h; k++) { //process of combining two sorted arrays
-        if(arr1[i]<=arr2[j])
-        arr[k]=arr1[i++];
-        else
-        arr[k]=arr2[j++];
-    }
-    return 0;
-}
-
-int merge_sort(int arr[],int low,int high)
-{
-    int mid;
-    if(low<high) {
-        mid=(low+high)/2;
-
-        merge_sort(arr,low,mid);
-        merge_sort(arr,mid+1,high);
-        merge(arr,low,mid,high);
-    }
-    return 0;
-}
-
