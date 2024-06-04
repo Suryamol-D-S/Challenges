59cf4777f771855cfdf3eb73ca287d021969ab73
Suryamol <suyamol.ds@mettlenetworks.com>
2024-06-04 10:28:55 +0530

    Description - Inserted the detailed description of program with Output for the reference in 01-quick-sort.c

--- challanges/01-quick-sort.c
+++ challanges/01-quick-sort.c
@@ -1,4 +1,28 @@
-// Quick sort with recursion
+/* Quick sort with recursion 
+   =========================
+   Quicksort is a divide-and-conquer algorithm. It works by selecting a 'max' 
+   element from the array and partitioning the other elements into two sub-arrays, 
+   according to whether they are less than or greater than the max.
+   -
+   -
+   -
+   Output :-
+   $ c99 -Wall -o 01-quick-sort 01-quick-sort.c -lm
+   $ ./01-quick-sort 
+   Enter the number of elements in the array: 
+   5
+   Enter the elements of the array: 
+   Element - 0: 3
+   Element - 1: 2
+   Element - 2: 4
+   Element - 3: 5
+   Element - 4: 1
+   The sorted array is: 
+   1
+   2
+   3
+   4
+   5*/
 
 #include <stdio.h>
 
