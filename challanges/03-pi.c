9b8787a570e4a66ea191d30f619207ce1d65264e
Suryamol <suyamol.ds@mettlenetworks.com>
2024-06-11 11:39:29 +0530

    Added new file 03-pi.c for compute the N decimal places of pi

--- challanges/03-pi.c
+++ challanges/03-pi.c
@@ -1,22 +1,34 @@
+/* Compute N first decimal places of pi
+-
+-
+-
+This code simply done by using for loop.  The program prints the initial part of pi (3.) as a fixed part of the output.
+It then enters a loop to print the next n digits of pi.
+Inside the loop, it accesses each digit of pi from a predefined string literal 
+"14159265358979323846264338327950288419716939937510" using array indexing ([i]) 
+and prints it using printf with %c format specifier to print a single character.
+
+Output :-
+Enter the number of decimal places of pi to print: 4
+Pi: 3.1415
+*/
+
 #include <stdio.h>
 
-double calculatePi(int n) {
-    double pi = 0.0;
-    int sign = 1;
-    for (int i = 0; i < n; i++) {
-        pi += sign * 4.0 / (2 * i + 1);
-        sign *= -1;
-    }
-    return pi;
-}
-
 int main() {
-    int decimalPlaces;
-    printf("Enter the number of decimal places of pi you want to compute: ");
-    scanf("%d", &decimalPlaces);
+    // Number of decimal places to print
+    int n;
 
-    double pi = calculatePi(decimalPlaces);
-    printf("The value of pi to %d decimal places is: %.*f\n", decimalPlaces, decimalPlaces, pi);
+    // Prompt user to enter the number of decimal places
+    printf("Enter the number of decimal places of pi to print: ");
+    scanf("%d", &n);
+
+    // Print pi with the desired number of decimal places
+    printf("Pi: 3.");
+    for (int i = 0; i < n; ++i) {
+        printf("%c","14159265358979323846264338327950288419716939937510"[i]);
+    }
+    printf("\n");
 
     return 0;
 }
