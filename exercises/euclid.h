f3d1327df26314e4138770409a022a235f4a5f78
Suryamol <suyamol.ds@mettlenetworks.com>
2024-06-14 18:11:32 +0530

    Added new file euclid.h

---
+++ exercises/euclid.h
@@ -0,0 +1,14 @@
+#ifndef EUCLID_H
+#define EUCLID_H
+
+// Function to calculate the greatest common divisor (GCD)
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+#endif
