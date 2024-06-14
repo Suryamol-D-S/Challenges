9e94806b4d15deb5ae159d6c75d832c69c3640d1
Suryamol <suyamol.ds@mettlenetworks.com>
2024-06-14 08:56:52 +0530

    Added new file 07-rat-get-prod.c

---
+++ exercises/07-rat-get-prod.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Structure to represent a rational number
+typedef struct {
+    int numerator;
+    int denominator;
+} Rational;
+
+// Function to multiply two rational numbers
+Rational rat_get_prod(Rational a, Rational b) {
+    Rational result;
+    result.numerator = a.numerator * b.numerator;
+    result.denominator = a.denominator * b.denominator;
+    return result;
+}
+
+int main() {
+    Rational a, b;
+
+    // Input for the first rational number
+    printf("Enter the numerator and denominator of the first rational number (separated by a space): ");
+    scanf("%d %d", &a.numerator, &a.denominator);
+
+    // Input for the second rational number
+    printf("Enter the numerator and denominator of the second rational number (separated by a space): ");
+    scanf("%d %d", &b.numerator, &b.denominator);
+
+    // Multiply the rational numbers
+    Rational product = rat_get_prod(a, b);
+
+    // Print the result
+    printf("Product: %d/%d\n", product.numerator, product.denominator);
+
+    return 0;
+}
